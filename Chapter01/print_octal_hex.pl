36b37bb2dac544fa82e8cd3861c30018ef5dc6c3
Shanley Philip <sphilip@mymail.mines.edu>
2011-07-23 13:53:00 -0600

    Can print octal digits by adding 0 before number and using 0x before hex digits

---
+++ Chapter01/print_octal_hex.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+
+# placing zero in front of some number means
+# that following digits are in octal form
+print 06301;
+print "\n";
+
+# placing zero x in front of some number means
+# that following digits are in hex
+print 0xBEEF;
+print "\n";
