ab89a1d469b22c0a61d56548e7148cbed3674df0
Shanley Philip <sphilip@mymail.mines.edu>
2011-07-23 14:00:15 -0600

    learned that unicode in \x{zzzz} prints that character to screen

--- Chapter01/print_octal_hex.pl
+++ Chapter01/print_octal_hex.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 
 # placing zero in front of some number means
 # that following digits are in octal form
@@ -9,3 +9,7 @@ print "\n";
 # that following digits are in hex
 print 0xBEEF;
 print "\n";
+
+# placing the unicode character in form  \x{zzzz} prints 
+# that unicode to the screen
+print "\x{10907}\n";
